ff1071a88f4ecd5d45a03b0e8705d217fffab25a
gustavo.tadei <gustavo.tadei@unir.br>
2020-03-30 17:17:17 -0400

    4 - Spring MVC II | 2 - Criação do Serviço Rest | 1 - Trabalhando com JSON - OK, funcionandp

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -87,6 +88,14 @@ public class ProdutosController {
         modelAndView.addObject("produto", produto);
         
         return modelAndView;
+    }
+    
+    @RequestMapping(name = "detalheProdutoJson", value = "/{id}")
+    @ResponseBody
+    public Produto detalheJson(@PathVariable("id") Integer id) {        
+        Produto produto = produtoDao.find(id);
+        
+        return produto;
     }
     
 }
