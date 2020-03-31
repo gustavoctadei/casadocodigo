684b03087cb304e5a50abc70f4e4ce2caca0e6e3
gustavo.tadei <gustavo.tadei@unir.br>
2020-03-31 14:58:25 -0400

    4 - Spring MVC II | 4 - Spring Security | 2 - Autorizando acesso - OK, funcionando

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -5,6 +5,8 @@
  */
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
@@ -15,5 +17,21 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 
 @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        
+        //A ordem das regras abaixo importa. Desta forma, é interessante primeiro fazer os bloqueios e depois liberar o restante
+        http.authorizeRequests()
+                .antMatchers("/produtos/form").hasRole("ADMIN")
+                .antMatchers("/carrinho").permitAll()
+                .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+                //.antMatchers(HttpMethod.GET, "/produtos").permitAll()
+                .antMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
+                .antMatchers("/produtos/**").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
     
 }
