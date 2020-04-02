aabe74766f25ad4f92450780fcf78698378681d0
gustavo.tadei <gustavo.tadei@unir.br>
2020-04-02 17:40:49 -0400

    4 - Spring MVC II | 7 - Testando a aplicação | 5 - Testando Controllers - OK, funcionando

---
+++ src/test/java/br/com/casadocodigo/loja/controllers/ProdutosControllerTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.casadocodigo.loja.controllers;
+
+import br.com.casadocodigo.loja.conf.AppWebConfiguration;
+import br.com.casadocodigo.loja.conf.DataSourceConfigurationTest;
+import br.com.casadocodigo.loja.conf.JPAConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ *
+ * @author Gustavo
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {JPAConfiguration.class, AppWebConfiguration.class, DataSourceConfigurationTest.class})
+@ActiveProfiles("test")
+public class ProdutosControllerTest {
+    
+    @Autowired
+    private WebApplicationContext wac;
+    
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+    
+    @Test
+    public void deveRetornarParaHomeComOsLivros() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("produtos"))
+                .andExpect(MockMvcResultMatchers
+                        .forwardedUrl("/WEB-INF/views/home.jsp"));
+    }
+    
+}
